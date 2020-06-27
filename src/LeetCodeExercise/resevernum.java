f8bcb032c7e7e9cdbb0fa933cbcc424ae4106ebd
yjlhz <2386987985@qq.com>
2020-06-27 15:44:44 +0800

    翻转整数算法练习

---
+++ src/LeetCodeExercise/resevernum.java
@@ -0,0 +1,17 @@
+package LeetCodeExercise;
+
+public class resevernum {
+    public static void main(String[] args){
+        resevernum rs = new resevernum();
+        System.out.println(rs.reserve(123));
+    }
+
+    public int reserve(int x){
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return (int) res == res ? (int) res : 0;
+    }
+}
