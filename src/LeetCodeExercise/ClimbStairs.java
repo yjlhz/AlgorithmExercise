913f1797d5699e8512a15706ce6651c5216d765d
yjlhz <2386987985@qq.com>
2020-09-24 00:31:58 +0800

    leetcode算法练习

--- src/LeetCodeExercise/ClimbStairs.java
+++ src/LeetCodeExercise/ClimbStairs.java
@@ -2,10 +2,12 @@ package LeetCodeExercise;
 
 public class ClimbStairs {
     public static void main(String[] args) {
-        System.out.println();
-    }
+        int[] num = {1,2,3,5};
+        System.out.println(climbStairs(55));
+        System.out.println(runningSum(num));
 
-    public int climbStairs(int n) {
+    }
+    public static int climbStairs(int n) {
         if (n == 1){
             return 1;
         }
@@ -19,6 +21,17 @@ public class ClimbStairs {
             r = p + q;
         }
         return r;
+    }
+
+
+    public static int[] runningSum(int[] nums) {
+        if (nums.length == 1){
+            return nums;
+        }
+        for (int i = 1;i < nums.length;i++){
+            nums[i] = nums[i-1] + nums[i];
+        }
+        return nums;
     }
 
 }
