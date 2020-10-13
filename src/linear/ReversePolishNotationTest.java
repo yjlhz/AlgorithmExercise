87c69000a4c72a7134bb9ee2f7bfdcc188ebd653
yjlhz <2386987985@qq.com>
2020-10-14 02:18:55 +0800

    用栈的方法求逆波兰表达式

---
+++ src/linear/ReversePolishNotationTest.java
@@ -0,0 +1,50 @@
+package linear;
+
+public class ReversePolishNotationTest {
+    public static void main(String[] args) {
+        String[] notation = {"3","17","15","-","*","18","6","/","+"};
+        int result = caculate(notation);
+        System.out.println(result);
+    }
+
+    public static int caculate(String[] notation) {
+        Stack<Integer> oprands = new Stack<>();
+        for (int i = 0;i<notation.length;i++){
+            String curr = notation[i];
+            Integer o1;
+            Integer o2;
+            Integer result;
+            switch (curr){
+                case "+":
+                    o1 = oprands.pop();
+                    o2 = oprands.pop();
+                    result = o1+o2;
+                    oprands.push(result);
+                    break;
+                case "-":
+                    o1 = oprands.pop();
+                    o2 = oprands.pop();
+                    result = o2-o1;
+                    oprands.push(result);
+                    break;
+                case "*":
+                    o1 = oprands.pop();
+                    o2 = oprands.pop();
+                    result = o1*o2;
+                    oprands.push(result);
+                    break;
+                case "/":
+                    o1 = oprands.pop();
+                    o2 = oprands.pop();
+                    result = o2/o1;
+                    oprands.push(result);
+                    break;
+                default:
+                    oprands.push(Integer.parseInt(curr));
+                    break;
+            }
+        }
+        int result = oprands.pop();
+        return result;
+    }
+}
