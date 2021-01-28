416d270968ce841dafeaf78bf6fed69f4a9345e8
yjlhz <2386987985@qq.com>
2021-01-28 18:54:39 +0800

    算法练习

---
+++ src/LeetCodeExercise/PathSum.java
@@ -0,0 +1,36 @@
+package LeetCodeExercise;
+
+import java.util.HashMap;
+
+public class PathSum {
+     public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, 1);
+        return helper(root, map, sum, 0);
+    }
+
+    int helper(TreeNode root, HashMap<Integer, Integer> map, int sum, int pathSum){
+        int res = 0;
+        if(root == null) return 0;
+
+        pathSum += root.val;
+        res += map.getOrDefault(pathSum - sum, 0);
+        map.put(pathSum, map.getOrDefault(pathSum, 0) + 1);
+        res = helper(root.left, map, sum, pathSum) + helper(root.right, map, sum, pathSum) + res;
+        map.put(pathSum, map.get(pathSum) - 1);
+        return res;
+    }
+
+}
